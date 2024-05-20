97cc89e213be1dc8575a9f7bf2258d4255765d18
wsl5300 <shanglingwu35@gmail.com>
2024-05-20 14:01:30 +0800

    first

---
+++ yolov8_car.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+# Load a pretrained YOLOv8n model
+model = YOLO("yolov8n-seg.pt")
+
+# Define path to the image file
+source = "/Users/wsl/work/mvc/week4/ultralytics/argoverse.mp4"
+
+# Run inference on the source
+results = model(source, save=True, classes=2) # list of Results objects
