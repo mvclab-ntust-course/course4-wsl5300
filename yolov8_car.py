497ab292cb0d0bbbfb9cb38facafc239f5d2b532
wsl5300 <145185075+wsl5300@users.noreply.github.com>
2024-05-20 15:15:09 +0800

    Update yolov8_car.py
    change path

--- yolov8_car.py
+++ yolov8_car.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n-seg.pt")
 
 # Define path to the image file
-source = "/Users/wsl/work/mvc/week4/ultralytics/argoverse.mp4"
+source = "/Users/wsl/work/mvc/week4/argoverse.mp4"
 
 # Run inference on the source
 results = model(source, save=True, classes=2) # list of Results objects
